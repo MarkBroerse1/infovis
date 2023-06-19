bf32aebd0de329ec36aa5545e46315ccce590514
Mark Broerse <mark.broerse@student.uva.nl>
2023-06-19 13:30:27 +0200

    full_Data.csv is de merge van de datasets

--- docs/data_reading.py
+++ docs/data_reading.py
@@ -5,3 +5,10 @@ happy = pd.read_csv('Happy.csv')
 
 def data_frame(input):
     return pd.DataFrame.head(input)
+
+
+# Merge the two dataframes based on 'country' and 'year'
+merged_data = pd.merge(hdi, happy, on=['Country', 'Year'])
+
+# Save the merged data to full_data.csv
+merged_data.to_csv('full_data.csv', index=False)
